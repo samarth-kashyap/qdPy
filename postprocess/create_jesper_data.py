BUGFIX: successfully reproduced the (1 - 1/ell) scaling of a1-coefficient for constant rotation rate; the complete radial grid needs to be considered, not just truncated at r=1.0; Yet to understand why this is the case

postprocess/create_jesper_data.py
@@ -1,26 +1,28 @@
 import numpy as np
 
-datadir = "/scratch/g.samarth/qdPy/new_freqs_jesper"
+datadir = "/scratch/g.samarth/qdPy/new_freqs_w135"
 
 data = np.loadtxt("/home/g.samarth/Woodard2013/WoodardPy/HMI/hmi.6328.36")
 lendata = data.shape[0]
 ells = data[:, 0].astype('int')
 enns = data[:, 1].astype('int')
-sigs = data[:, 48:56]
 
-nmax = 5
-mask_nmax = enns <= 8
+# max_splits = 6
+max_splits = 36
+sigs = data[:, 48:48+max_splits]
+
+nmax = 7
+mask_nmax = enns <= nmax
 ells = ells[mask_nmax]
 enns = enns[mask_nmax]
 sigs = sigs[mask_nmax, :]
 lendata = mask_nmax.sum()
 
-max_splits = 6
 split_idxs = np.arange(1, max_splits+1, 2)
 #split_idxs = np.arange(1, max_splits+1, 2)
 
-fqd = open("qdpt.jesper.36.splits", "w")
-fd = open("dpt.jesper.36.splits", "w")
+fqd = open("qdpt.w135.36.splits", "w")
+fd = open("dpt.w135.36.splits", "w")
 
 for i in range(lendata):
     ell, enn = ells[i], enns[i]

qdpt.py
@@ -35,7 +35,12 @@ args = parser.parse_args()
 
 
 # {{{ Reading global variables
-rmin, rmax = 0.0, 1.0
+rmin, rmax = 0.0, 1.2
+# setting rmax as 1.2 because the entire r array needs to be used
+# in order to reproduce
+# (1) the correct normalization
+# (2) a1 = \omega_0 ( 1 - 1/ell ) scaling
+
 # (Since we are using lmax = 300, 0.45*300 \approx 150)
 SMAX = 5      # maximum s for constructing supermatrix
 FWINDOW = 150   # microHz
@@ -113,20 +118,25 @@ fdpt *= gvar.OM * 1e6
 fqdpt *= gvar.OM * 1e6
 
 # dirname = "new_freqs_half"
-dirnamenew = "new_freqs_jesper"
+# dirnamenew = "new_freqs_430"
+dirnamenew = "new_freqs_w135"
 
 np.save(f'{gvar.datadir}/{dirnamenew}/qdpt_{args.n0:02d}_{args.l0:03d}.npy', fqdpt)
 np.save(f'{gvar.datadir}/{dirnamenew}/dpt_{args.n0:02d}_{args.l0:03d}.npy', fdpt)
 # store_offset()
 
+
+# converting to nHz before computing splitting coefficients
+fdpt *= 1e3
+fqdpt *= 1e3
+
 ritz_degree = min(args.l0//2+1, 36)
 rlp = RL.ritzLavelyPoly(args.l0, ritz_degree)
 
-# acoeffs_qdpt = get_RL_coeffs(rlp, fqdpt[::-1])*1e3
-# acoeffs_dpt = get_RL_coeffs(rlp, fdpt[::-1])*1e3
-acoeffs_qdpt = get_RL_coeffs(rlp, fqdpt)*1e3
-acoeffs_dpt = get_RL_coeffs(rlp, fdpt)*1e3
+acoeffs_qdpt = get_RL_coeffs(rlp, fqdpt)
+acoeffs_dpt = get_RL_coeffs(rlp, fdpt)
 
+# converting nu_{n\ell} back to muHz
 acoeffs_qdpt[0] /= 1e3
 acoeffs_dpt[0] /= 1e3
 