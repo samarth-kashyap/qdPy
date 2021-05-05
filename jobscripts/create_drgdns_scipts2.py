d02c3b7328ea625a7270500433670493063a8061
srijaniiserprinceton <sbdas@princeton.edu>
2021-05-04 23:29:18 -0400

    Adding miscellaneous jobscripts

--- jobscripts/create_drgdns_scipts2.py
+++ jobscripts/create_drgdns_scipts2.py
@@ -7,17 +7,17 @@ parser.add_argument("--n", help='radial order',
                     type=int)
 args = parser.parse_args()
 
-pythonpath = "/home/g.samarth/anaconda3/bin/python"
-execpath = "/home/g.samarth/qdPy/qdpt.py"
+pythonpath = "/home/sbdas/.conda/envs/helio/bin/python"
+execpath = "/home/sbdas/Research/Helioseismology/qdPy/qdpt.py"
 
-hmi_data = np.loadtxt("/home/g.samarth/Woodard2013/WoodardPy/HMI/hmi.6328.36")
+hmi_data = np.loadtxt("/scratch/gpfs/sbdas/Helioseismology/get-solar-eigs/efs_Antia/data/HMI/hmi.6328.36")
 
 n = args.n
 mask_n = hmi_data[:, 1] == n
 l_arr = hmi_data[mask_n, 0]
 len_ell = len(l_arr)
 
-filename = f"/home/g.samarth/qdPy/jobscripts/ipjobs_{n:02d}.sh"
+filename = f"/home/sbdas/Research/Helioseismology/qdPy/jobscripts/ipjobs_{n:02d}.sh"
 
 with open(filename, "w") as f:
     for i in range(len_ell):
